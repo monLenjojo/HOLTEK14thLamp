5535b8ed206aa08b73aded399e1b76c31e6bc955
monLenjojo <xo3374ox@gmail.com>
2019-05-30 17:52:19 +0800

    add login way than use facebook

--- app/src/main/java/com/tsc/holtek14th/LoginActivity.java
+++ app/src/main/java/com/tsc/holtek14th/LoginActivity.java
@@ -1,34 +1,105 @@
 package com.tsc.holtek14th;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String TAG = LoginActivity.class.getSimpleName();
+    private static final String FBTAG = LoginActivity.class.getSimpleName()+" Facebook-auth";
     private FirebaseAuth auth;
     private EditText edUserId;
     private EditText edPasswd;
     private Boolean state;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         auth = FirebaseAuth.getInstance();
 
         edUserId = findViewById(R.id.edUserId);
         edPasswd = findViewById(R.id.edPasswd);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+
+        LoginButton loginButton = findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email", "public_profile");
+        callbackManager = CallbackManager.Factory.create();
+        auth = FirebaseAuth.getInstance();
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                AccessToken token = loginResult.getAccessToken();
+                AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+                auth.signInWithCredential(credential).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = auth.getCurrentUser();
+                            Log.d(FBTAG, "login onComplete: " + user.getDisplayName());
+                        }else {
+                            Log.d(FBTAG, "login is Fail");
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+        Profile fbProfile = Profile.getCurrentProfile();
+
+        AccessTokenTracker accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                if (currentAccessToken==null) {
+                    auth.signOut();
+                    Log.d(FBTAG, "onCurrentAccessTokenChanged: ");
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        callbackManager.onActivityResult(requestCode,resultCode,data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     public void login(View view){
--- app/src/main/java/com/tsc/holtek14th/recyclerFunction/MyLibraryRecyclerFunction.java
+++ app/src/main/java/com/tsc/holtek14th/recyclerFunction/MyLibraryRecyclerFunction.java
@@ -3,11 +3,13 @@ package com.tsc.holtek14th.recyclerFunction;
 import android.content.Context;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.tsc.holtek14th.R;
 import com.tsc.holtek14th.javaBean.AllStoryFormat;
 import com.tsc.holtek14th.recyclerAdapter.MyLibraryRecyclerAdapter;
 
@@ -22,22 +24,28 @@ public class MyLibraryRecyclerFunction {
     String userId;
     ArrayList<AllStoryFormat> arrayList = new ArrayList();
 
-    public MyLibraryRecyclerFunction(Context context, RecyclerView recyclerView, String userId) {
+    public MyLibraryRecyclerFunction(Context context, final RecyclerView recyclerView, @Nullable String userId) {
         this.context = context;
         this.recyclerView = recyclerView;
         this.userId = userId;
         FirebaseFirestore firestore = FirebaseFirestore.getInstance();
-
-        firestore.collection("userData")
+        if (userId != null) {
+            firestore.collection("userData")
                 .document(userId)
                 .addSnapshotListener(new EventListener<DocumentSnapshot>() {
                     @Override
                     public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
-                        AllStoryFormat data = documentSnapshot.toObject(AllStoryFormat.class);
-                        arrayList.add(data);
-                        upData();
+                        Log.d("TAG", "onEvent: " + documentSnapshot.getData());
+                        if (documentSnapshot.getData() != null) {
+                            AllStoryFormat data = documentSnapshot.toObject(AllStoryFormat.class);
+                            arrayList.add(data);
+                            upData();
+                        }else {
+                            recyclerView.setBackgroundResource(R.drawable.login_bg_space);
+                        }
                     }
                 });
+        }
     }
 
     private void upData() {
